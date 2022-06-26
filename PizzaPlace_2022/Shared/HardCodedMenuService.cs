b65ad5d2c864845ab73c1311899307a351fa2ae4
unknown <nhung123k31@gmail.com>
2022-06-26 20:28:57 +0700

    first commit

---
+++ PizzaPlace_2022/Shared/HardCodedMenuService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace_2022.Shared {
+    public class HardCodedMenuService : IMenuService {
+        public ValueTask<Menu> GetMenu() => new ValueTask<Menu>(
+            new Menu {
+                Pizzas = new List<Pizza> {
+                    //new Pizza(1, "Pepperoni", 8.99M, Spiciness.Spicy),
+                    //new Pizza(2, "Margarita", 7.99M, Spiciness.None),
+                    ////new Pizza(3, "Diabolo", 9.99M, Spiciness.Hot)
+                    new Pizza(4, "Áo Phông", 45, Spiciness.ao1),
+
+                    new Pizza(5, "Váy Công Chúa", 50, Spiciness.ao2),
+                     new Pizza(6, "Áo Bông Hoa", 60, Spiciness.ao3),
+                      new Pizza(7, "Chân Váy", 49, Spiciness.vay),
+                      new Pizza(8, "Áo", 49, Spiciness.ao4)
+                }
+            });
+    }
+}
+
---
+++ PizzaPlace_2022/Shared/Spiciness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace_2022.Shared {
+    public enum Spiciness {
+        None,
+        Spicy,
+        Hot,
+       // hoa1,
+        //hoa2,
+        //hoa3,
+        //hoa4
+        ao1,
+        ao2,
+            ao3,
+            ao4,
+            vay
+    }
+}
